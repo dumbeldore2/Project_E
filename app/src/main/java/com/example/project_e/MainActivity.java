8722511ba15fc6cc898d77f45d92df240405dbbc
dumbeldore2 <60613998+dumbeldore2@users.noreply.github.com>
2021-10-28 22:18:52 +0200

    de eerste pagina is aangemaakt ,

--- app/src/main/java/com/example/project_e/MainActivity.java
+++ app/src/main/java/com/example/project_e/MainActivity.java
@@ -1,14 +1,40 @@
 package com.example.project_e;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
+    //de int dat beslist hoelang deze pagina op blijft
+    private static int SPLASH_TIME_OUT = 4000;
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        );
+        getWindow().setNavigationBarColor(getResources().getColor(R.color.color_2_light));
+        getWindow().setStatusBarColor(getResources().getColor(R.color.color_1_dark));
+
+        //de spash
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,MainActivity2.class);
+                startActivity(intent
+                );
+                finish();
+            }
+        },SPLASH_TIME_OUT);
     }
 }
