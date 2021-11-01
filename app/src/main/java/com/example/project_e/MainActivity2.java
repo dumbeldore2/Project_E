60c9a2525eddd693f83d0dfb76064d0946a00ee9
PC001 laptop <yago.engels@gmail.com>
2021-11-01 23:22:22 +0100

    er is iets kei vaag an de hand maar ik weet niet wat ptn das echt vreemd

--- app/src/main/java/com/example/project_e/MainActivity2.java
+++ app/src/main/java/com/example/project_e/MainActivity2.java
@@ -3,9 +3,12 @@ package com.example.project_e;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -53,5 +56,18 @@ public class MainActivity2 extends AppCompatActivity {
 
         //arraylist adapteren
         listView.setAdapter(mainActivity2_list);
+
+        //funties callen
+        click_fun();
+    }
+
+    public void click_fun(){
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getApplicationContext(),MainActivity3.class);
+                startActivity(intent);
+            }
+        });
     }
 }
